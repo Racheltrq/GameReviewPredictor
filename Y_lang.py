5324f9f9a647be12377065044845743c7e949b9d
Rachel Tao <rt1726@nyu.edu>
2020-05-03 02:14:43 -0400

    fix incorrect rows in XYmat and XYmat_lang

--- Y_lang.py
+++ Y_lang.py
@@ -1,19 +1,25 @@
 import csv
+import copy
 
 with open("y_raw.csv", 'r') as infile, open("Xmat_lang.csv") as infile1, open("XYmat_lang.csv", 'w') as outfile:
 	r = csv.DictReader(infile)
 	r1 = csv.DictReader(infile1)
 	fieldnames = next(r1)
+	xrow = copy.deepcopy(fieldnames)
+	#print(fieldnames)
 	fieldnames.pop("name")
 	header = ["name", "reviews"]
 	for i in fieldnames:
 		header.append(i)
 	w = csv.DictWriter(outfile, fieldnames = header)
 	w.writeheader()
-
+	i = 0
 	for row in r:
 		
 		if row["reviews"] != "NaN":
-			xrow = next(r1)
+			if i != 0:
+				xrow = next(r1)
 			xrow["reviews"] = row["reviews"]
 			w.writerow(xrow)
+			i += 1
+
